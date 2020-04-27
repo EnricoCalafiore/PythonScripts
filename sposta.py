d95842e44de357f9c5ef74478580b65224bcc621
Enrico Calafiore <33897298+EnricoCalafiore@users.noreply.github.com>
2020-04-27 11:52:44 +0200

    Update sposta.py

--- sposta.py
+++ sposta.py
@@ -1,8 +1,8 @@
 import os
 import shutil
-sourcepath=r'C:\Users\calaf\Downloads'
+sourcepath=r'C:\Users\*****\Downloads'
 sourcefiles = os.listdir(sourcepath)
-destinationpath = r'C:\Users\calaf\Downloads\PDF_FILES'
+destinationpath = r'C:\Users\****\Downloads\PDF_FILES'
 for file in sourcefiles:
     if file.endswith('.pdf'):
         shutil.move(os.path.join(sourcepath,file), os.path.join(destinationpath,file))
